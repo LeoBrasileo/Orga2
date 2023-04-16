7bc5908cb9c485ef121f11782d3a16850bbbb8db
nicocarrasco <ncarrasco@dc.uba.ar>
2023-04-16 15:49:19 -0300

    ej1 labo simd

--- LaboSimd/src/ej1y2/checkpoint1.asm
+++ LaboSimd/src/ej1y2/checkpoint1.asm
@@ -1,6 +1,6 @@
 section .rodata
 mascara_1:
-  db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+  db 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
 
 section .text
 
@@ -9,15 +9,50 @@ global invertirBytes_asm
 ; void invertirBytes_asm(uint8_t* p, uint8_t n, uint8_t m)
 
 invertirBytes_asm:
-	movdqa xmm0, [rdi]      ; xmmo = p[0] | p[1] | p[2] | p[3] ...
-    pxor xmm1, xmm1       	; xmm1 = 0 | 0 | 0 | 0 ...
-	por xmm1, [mascara_1]   ; xmm1 = 0 | 1 | 2 | 3 ...
-    mov eax, esi            ; eax = n
-    mov ebx, edx            ; ebx = m
-	;intercambio n por m en xmm1
-	movdqa xmm2, xmm1
-	
+    push rbp
+    mov rbp, rsp
 
-    pshufb xmm0, xmm1
-    movdqa [rdi], xmm0
-	ret
+    movdqu xmm0, [rdi]      ; xmmo = p[0] | p[1] | p[2] | p[3] ...
+    movdqu xmm1, [mascara_1]   ; xmm1 = 0 | 1 | 2 | 3 ...
+
+    pxor xmm2, xmm2 
+    pxor xmm4, xmm4
+    pinsrb xmm2, sil, 0 ; inserto n en el byte 0 de xmm2
+    pinsrb xmm4, dil, 0 ; inserto m en el byte 0 de xmm4
+    mov r8b, sil ; r8b = n
+
+    .shift1: ; desplazo a n y m, n bytes a la izquierda
+    cmp r8b, 0
+    je .break1
+    pslldq xmm2, 1 ;shift left 1 byte
+    pslldq xmm4, 1 ; idem
+    dec r8b
+    jmp .shift1
+    .break1
+
+    pxor xmm5, xmm5
+    pxor xmm3, xmm3
+    pinsrb xmm3, dil , 0 ; inserto m en el byte 0 de xmm3
+    pinsrb xmm5, sil, 0 ; inserto n en el byte 0 de xmm5
+    mov r8b, dil ; r8b = m
+
+    .shift2: ; desplazo a n y m, m bytes a la izquierda
+    cmp r8b, 0
+    je .break2
+    pslldq xmm3, 1 ;shift left 1 byte
+    pslldq xmm5, 1 ;idem
+    dec r8b
+    jmp .shift2
+    .break2
+
+    pxor xmm1, xmm2 ; borro a n de la mascara
+    pxor xmm1, xmm3 ; borro a m de la mascara
+    por xmm1, xmm4 ; inserto a n en la posicion m en xmm3
+    por xmm1, xmm5 ; inserto a m en la posicion n en xmm3
+    pshufb xmm0, xmm1 ; shuffle con la mascara generada
+
+    movdqu [rdi], xmm0 ; guardo el resultado en p
+
+    mov rsp, rbp
+    pop rbp
+    ret
--- LaboSimd/src/ej1y2/main.c
+++ LaboSimd/src/ej1y2/main.c
@@ -10,9 +10,9 @@ int main (void){
 	/* Acá pueden realizar sus propias pruebas */
 	uint8_t* bytes_c = (uint8_t*) malloc(16);
 	for (int k = 0; k < 16; k++)
-            bytes_c[k] = k+6;
+            bytes_c[k] = k;
 
-	invertirBytes_asm(bytes_c, 0, 15);
+	invertirBytes_asm(bytes_c, 2, 10);
 
 	for(uint32_t j = 0; j < 16; j++){
         printf("bytes_c[%2u] = %3u\t", j, bytes_c[j]);
