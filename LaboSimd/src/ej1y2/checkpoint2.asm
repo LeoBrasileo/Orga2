2631176beeb770235fce6b524a6883a79eff571a
nicocarrasco <ncarrasco@dc.uba.ar>
2023-04-19 11:36:25 -0300

    Terminado checkpoint 2 (hay leaks)

--- LaboSimd/src/ej1y2/checkpoint2.asm
+++ LaboSimd/src/ej1y2/checkpoint2.asm
@@ -14,33 +14,42 @@ checksum_asm:
 	push rbp
 	mov rsp, rbp
 
-	mov rcx, esi
-	mov rbx, rdi ;
+	mov ecx, esi ; ecx = n
+	mov rbx, rdi ; pasamos la direccion de array a rbx
 	movdqu xmm5, [ocho]
 	movdqu xmm8, [unos]
 
 
 	.ciclo:
-	pmovzxwd xmm0, [rbx] ; muevo la primera mitad de Aij a xmm0
-	pmovzxwd xmm1, [rbx + 16]	; muevo la primera mitad de Bij a xmm1
-	pmovzxwd xmm2, [rbx + 8] ; muevo la segunda mitad de Aij a xmm0
-	pmovzxwd xmm3, [rbx + 24] ; muevo la segunda mitad de Bij a xmm0
+	
+	; partimos los Aij, Bij, Cij en dos partes
+
+	pmovzxwd xmm0, [rbx] ; muevo la primera mitad de Aij a xmm0 extendiendola a 32 bits
+	pmovzxwd xmm1, [rbx + 16]	; muevo la primera mitad de Bij a xmm1 extendiendola a 32 bits
+	pmovzxwd xmm2, [rbx + 8] ; muevo la segunda mitad de Aij a xmm0 extendiendola a 32 bits
+	pmovzxwd xmm3, [rbx + 24] ; muevo la segunda mitad de Bij a xmm0 extendiendola a 32 bits
 	movdqu xmm6, [rbx + 32] ; muevo la primera mitad de Cij a xmm6
 	movdqu xmm7, [rbx + 48] ; muevo la segunda mitad de Cij a xmm7
+
+	; operaciones
 	paddw xmm0, xmm1 ; sumo las primeras mitades
 	paddw xmm2, xmm3 ; sumo las segundas mitades
 	pmuldq xmm0, xmm5 ; primera mitad x 8
 	pmuldq xmm1, xmm5 ; segunda mitad x 8
+
+	; comparaciones
 	pcmpeqd xmm6, xmm0 ; compara las operaciones de la primera mitad con la primera mitad de Cij
 	pcmpeqd xmm7, xmm1 ; compara las operaciones de la segunda mitad con la segunda mitad de Cij
 	pand xmm6, xmm7 ; chequea si ambas mitades son iguales
-	pcmpeqd xmm8, xmm6 ; chequea si el resultado es 1
-	; hay que ver como salir del ciclo cuando no es 1
-	add rbx, offset ; desplazamiento a la proxima terna
-	loop .ciclo
-	jmp fin
+	ptest xmm8, xmm6 ; chequea si el resultado es 1
 
-	.fin
+	; saltos
+	jnz .fin ; si no es 1, salta a fin
+	add rbx, offset ; desplazamiento a la proxima terna, rbx = rbx + 64
+	loop .ciclo
+	jmp .fin
+
+	.fin:
 	mov rsp, rbp
 	pop rbp
 	ret
