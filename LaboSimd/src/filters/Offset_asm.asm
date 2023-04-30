189a61c9f9f4487dba99e0479ce1051b675b914e
LeoBrasileo <leobraginski@gmail.com>
2023-04-30 01:26:29 -0300

    BORDE

--- LaboSimd/src/filters/Offset_asm.asm
+++ LaboSimd/src/filters/Offset_asm.asm
@@ -1,8 +1,10 @@
 section .rodata
 mascara_1:
   db 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
+
+; la mascara se define de a 4 bytes, en orden blue, red, green, alpha
 solo_alpha:
-	db 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0
+	db 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0xFF
 
 section .data
 	%define blue 0
@@ -26,22 +28,24 @@ Offset_asm:
 	push r13
 	push r14
 	push r15
-	sub rsp, 8
+
+	.todoNegro:
 
 	lea rbx, [rdi] ; rbx = src
 	lea r12, [rsi] ; r12 = dst
 	xor r13, r13 ; r13 = 0, guardamos en r13 el desplazamiento en src
 	xor r14, r14 ; r14 = 0, guardamos en r14 el desplazamiento en dst
 	mov r15, rcx ; r15 = height, guardamos en r15 la altura de la imagen
+	push rcx
 
 	xor rcx, rcx
 	mov rcx, r8 ; rcx = src_row_size
 
 	movdqu xmm15, [solo_alpha] ; pasamos la mascara a xmm15
 
-	.ciclo:
+	.cicloNegro:
 	cmp r15, 0 ; si r15 = 0 terminamos
-	je .fin
+	je .filtro
 
 	movdqu xmm0, [rbx] ; traemos 4 pixeles
 	pand xmm0, xmm15 ; AND con la mascara
@@ -53,17 +57,61 @@ Offset_asm:
 	add r13, offset_pixels ; sumamos 4 pixeles al desplazamiento total en src
 	add r14, offset_pixels ; sumamos 4 pixeles al desplazamiento total en dst
 
-	cmp r13, rcx ; si nos desplazamos el ancho de la imagen hacemos .sogFila sino seguimos con .ciclo
-	jl .ciclo
+	cmp r13, rcx ; si nos desplazamos el ancho de la imagen hacemos .sigFila sino seguimos con .ciclo
+	jl .cicloNegro
 	
-	.sigFila:
+	.sigFilaNegro:
 	xor r13, r13 ; reiniciamos el desplazamiento en src
 	xor r14, r14 ; reiniciamos el desplazamiento en dst
 	dec r15	
-	jmp .ciclo
+	jmp .cicloNegro
+
+	.filtro:
+	imul r9, 8 ; r9 = dst_row_size * 8
+
+	lea rbx, [rdi] ; rbx = src
+	lea r12, [rsi] ; r12 = dst
+	xor r13, r13 ; r13 = 0, guardamos en r13 el desplazamiento en src
+	xor r14, r14 ; r14 = 0, guardamos en r14 el desplazamiento en dst
+	pop rcx
+	mov r15, rcx ; r15 = height, guardamos en r15 la altura de la imagen
+
+	xor rcx, rcx
+	mov rcx, r8 ; rcx = src_row_size
+	sub rcx, offset_pixels*4 ; rcx = src_row_size - 8 pixels
+
+	add r12, r9 ; r12 = dst + dst_row_size * 8
+	add rbx, r9 ; rbx = src + dst_row_size * 8
+	add r12, offset_pixels*2 ; avanzo 8 pixeles
+	add rbx, offset_pixels*2
+	sub r15, 16 ; r15 = height - 16 pixeles, 8 arriba, 8 abajo
+
+	.cicloFiltro:
+	cmp r15, 0 ; si r15 = 0 terminamos
+	je .fin
+
+	movdqu xmm0, [rbx] ; traemos 4 pixeles
+	movdqu [r12], xmm0 ; guardamos los 4 pixeles en dst
+	
+	add rbx, offset_pixels ; avanzamos 4 pixeles de lectura en src
+	add r12, offset_pixels ; avanzamos 4 pixeles de escritura en dst
+
+	add r13, offset_pixels ; sumamos 4 pixeles al desplazamiento total en src
+	add r14, offset_pixels ; sumamos 4 pixeles al desplazamiento total en dst
+
+	cmp r13, rcx ; si nos desplazamos el ancho de la imagen hacemos cambio de fila sino seguimos con .ciclo
+	jl .cicloFiltro
+	
+	xor r13, r13 ; reiniciamos el desplazamiento en src
+	xor r14, r14 ; reiniciamos el desplazamiento en dst
+	dec r15
+	;acomodo borde
+	add rbx, offset_pixels*4 ; avanzamos 8 pixeles de lectura en src
+	add r12, offset_pixels*4 ; avanzamos 8 pixeles de escritura en dst
+	jmp .cicloFiltro
+
 	
 	.fin:
-	add rsp, 8
 	pop r15
 	pop r14
 	pop r13
