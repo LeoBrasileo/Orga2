5f2367a222c050603e568c7e43f20b8d5aadcf8e
LeoBrasileo <leobraginski@gmail.com>
2023-05-02 01:45:16 -0300

    verde

--- LaboSimd/src/filters/Sharpen_asm.asm
+++ LaboSimd/src/filters/Sharpen_asm.asm
@@ -1,7 +1,12 @@
 extern malloc
+extern free
+
+section .rodata
+mascara_azul:
+  db 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
 
 section .text
-	%define offset_pixels 16
+	%define offset_pixels 4
 	%define float_size 4
 	
 
@@ -18,13 +23,12 @@ Sharpen_asm:
 	push r13
 	push r14
 	push r15
-	sub rsp, 8
 
 	; cargada de registros iniciales
 	lea rbx, [rdi] ; rbx = src
 	lea r12, [rsi] ; r12 = dst
 	xor r13, r13 ; r13 = 0, guardamos en r13 el desplazamiento en src
-	xor r14, r14 ; r14 = 0, guardamos en r14 el desplazamiento en dst
+	xor r14, r14 ; r14 = 0, guardamos en r14 los valores de los pixeles en cada ciclo
 	mov r15, rcx ; r15 = height, guardamos en r15 la altura de la imagen
 
 
@@ -36,49 +40,91 @@ Sharpen_asm:
 	.llenarMatriz:
 	cmp rcx, 9
 	je .matrizLlena
-	mov DWORD [rax + rcx*float_size], -1
+	mov DWORD [rax + rcx*float_size], 0xBF800000 ; -1 en float
 	inc rcx
 	jmp .llenarMatriz
 
 	.matrizLlena:
-	mov DWORD [rax + 4*float_size], 9
-
+	mov DWORD [rax + 4*float_size], 0x40C00000 ; 9 en float
 
 	; rax = direccion de memoria de sharpen
 	; sharpen = | -1 | -1 | -1 |
 	;		    | -1 |  9 | -1 |
 	;		    | -1 | -1 | -1 |
 
+	sub r8, 2*offset_pixels ; r8 = src_row_size - 2 pixels de borde
 
-	xor rcx, rcx
-	mov rcx, r8 ; rcx = src_row_size
+	add r12, offset_pixels; avanzo 1 pixeles de inicializacion
+	add rbx, offset_pixels
+	sub r15, 2 ; r15 = height - 2 pixeles, 1 arriba, 1 abajo
+
+
+	pxor xmm0, xmm0 ; limpio xmm0 para usarlo como registro de tratamiento general
 
 	.filtro:
 	cmp r15, 0
 	je .fin
 
-	movdqu xmm0, [rbx] ; traemos 4 pixeles
+	; procesamiento de a 1 pixel
+	; azul = xmm1, verde = xmm2, rojo = xmm3
+	pxor xmm1, xmm1
+	pxor xmm2, xmm2
+	pxor xmm3, xmm3
 
-	;aca va la logica del filtro
+	mov rcx, 9
+	.recorrerMatriz:
+	movss xmm4, [rax + rcx*float_size] ; traemos valor de la matriz
 
-	movdqu [r12], xmm0 ; guardamos los 4 pixeles en dst
+	;AZUL
+	movzx r14d, BYTE [rbx]
+	cvtsi2ss xmm5, r14d ; pasamos a float
+	mulss xmm5, xmm4 ; multiplicamos
+	addss xmm1, xmm5
+
+	;VERDE
+	movzx r14d, BYTE [rbx+1]
+	cvtsi2ss xmm5, r14d ; pasamos a float
+	mulss xmm5, xmm4 ; multiplicamos
+	addss xmm2, xmm5
+
+	;ROJO
+	movzx r14d, BYTE [rbx+2]
+	cvtsi2ss xmm5, r14d ; pasamos a float
+	mulss xmm5, xmm4 ; multiplicamos
+	addss xmm3, xmm5
+	loop .recorrerMatriz
+
+	pslldq xmm1, 24
+	pslldq xmm2, 16
+	pslldq xmm3, 8
+	por xmm0, xmm1
+	por xmm0, xmm2
+	por xmm0, xmm3
+
+	packssdw xmm0, xmm0 ; pasamos a 16 bits
+	packsswb xmm0, xmm0 ; pasamos a 8 bits
+	pextrd r14d, xmm0, 0 ; guardamos en r14d el valor de la parte baja de xmm0
+	mov r14b, BYTE [rbx + 3] ; guardamos el valor de alpha en r14b
+
+	mov DWORD [r12 + r8 + 8 + 1], r14d ; guardo resultado en dst[i+1][j+1]
+
 	
-	add rbx, offset_pixels ; avanzamos 4 pixeles de lectura en src
-	add r12, offset_pixels ; avanzamos 4 pixeles de escritura en dst
+	add rbx, offset_pixels ; avanzamos 1 pixel de lectura en src
+	add r12, offset_pixels ; avanzamos 1 pixel de escritura en dst
 
-	add r13, offset_pixels ; sumamos 4 pixeles al desplazamiento total en src
-	add r14, offset_pixels ; sumamos 4 pixeles al desplazamiento total en dst
+	add r13, offset_pixels ; sumamos 1 pixel al desplazamiento total en src
 
-	cmp r13, rcx ; si nos desplazamos el ancho de la imagen hacemos .sigFila sino seguimos con .ciclo
+	cmp r13, r8 ; si nos desplazamos el ancho de la imagen hacemos .sigFila sino seguimos con .ciclo
 	jl .filtro
 
 	xor r13, r13 ; reiniciamos el desplazamiento en src
-	xor r14, r14 ; reiniciamos el desplazamiento en dst
 	dec r15	
+	;acomodo borde
+	add rbx, 2*offset_pixels ; avanzamos 2 pixeles de lectura en src, 1 para terminar la fila y 1 para empezar la siguiente
+	add r12, 2*offset_pixels ; avanzamos 2 pixeles de escritura en dst
 	jmp .filtro
 
 	.fin:
-	add rsp, 8
 	pop r15
 	pop r14
 	pop r13
