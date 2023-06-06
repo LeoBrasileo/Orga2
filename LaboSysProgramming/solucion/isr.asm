ad94c88cc152a7c74c8fc822aeffb45e5fafa073
cualquiercosa <gianfrancobtl@gmail.com>
2023-06-06 18:04:38 -0300

    Ag. labo de tareas

--- LaboSysProgramming/solucion/isr.asm
+++ LaboSysProgramming/solucion/isr.asm
@@ -8,13 +8,24 @@
 %include "print.mac"
 %define CS_RING_0_SEL    (1 << 3)
 
+%define SHARED_TICK_COUNT 0x1D000
 BITS 32
+
+sched_task_offset:     dd 0xFFFFFFFF
+sched_task_selector:   dw 0xFFFF
 
 ;; PIC
 extern pic_finish1
+
+;; Sched
+extern sched_next_task
 extern kernel_exception
 
-extern process_scancode
+;; Tasks
+extern tasks_tick
+extern tasks_screen_update
+extern tasks_syscall_draw
+extern tasks_input_process
 
 ;; Definición de MACROS
 ;; -------------------------------------------------------------------------- ;;
@@ -47,7 +58,8 @@ extern process_scancode
     push eax
     mov ax, ds
     push eax
-    push eax ; cs
+    mov ax, cs
+    push eax
 
     ; CREGS
     mov eax, cr4
@@ -62,7 +74,15 @@ extern process_scancode
     cmp edx, CS_RING_0_SEL
     je .ring0_exception
 
-    ;call ring3_exception
+    ; COMPLETAR (opcional):
+    ;   Si caemos acá es porque una tarea causó una excepción
+    ;   En lugar de frenar el sistema podríamos matar la tarea (o reiniciarla)
+    ;   ¿Cómo harían eso?
+    call kernel_exception
+    add esp, 10*4
+    popad
+
+    xchg bx, bx
     jmp $
 
 
@@ -120,23 +140,45 @@ ISRNE 20
 ;; Rutina de atención del RELOJ
 ;; -------------------------------------------------------------------------- ;;
 global _isr32
+; COMPLETAR: Implementar la rutina
 _isr32:
     pushad
-    call next_clock
+    ; 1. Le decimos al PIC que vamos a atender la interrupción
     call pic_finish1
+    call next_clock
+    ; 2. Actualizamos las estructuras compartidas ante el tick del reloj
+    call tasks_tick
+    ; 3. Realizamos el cambio de tareas en caso de ser necesario
+    call sched_next_task
+    cmp ax, 0
+    je .fin
+
+    str bx
+    cmp ax, bx
+    je .fin
+
+    mov word [sched_task_selector], ax
+    jmp far [sched_task_offset]
+
+    .fin:
+    ; 4. Actualizamos la "interfaz" del sistema en pantalla
+    call tasks_screen_update
     popad
     iret
 
 ;; Rutina de atención del TECLADO
 ;; -------------------------------------------------------------------------- ;;
 global _isr33
+; COMPLETAR: Implementar la rutina
 _isr33:
     pushad
+    ; 1. Le decimos al PIC que vamos a atender la interrupción
+    call pic_finish1
+    ; 2. Leemos la tecla desde el teclado y la procesamos
     in al, 0x60
     push eax
-    call process_scancode
-    pop eax
-    call pic_finish1
+    call tasks_input_process
+    add esp, 4
     popad
     iret
 
@@ -145,14 +187,19 @@ _isr33:
 ;; -------------------------------------------------------------------------- ;;
 
 global _isr88
+; Syscall para que una tarea dibuje en su pantalla
 _isr88:
-    mov eax, 0x58
-    iret
+  pushad
+  push eax
+  call tasks_syscall_draw
+  add esp, 4
+  popad
+  iret
 
 global _isr98
 _isr98:
-    mov eax, 0x62
-    iret
+  mov eax, 0x62
+  iret
 
 ; PushAD Order
 %define offset_EAX 28
